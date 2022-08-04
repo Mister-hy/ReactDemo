27408bb19e8a19a22c4d99d16f199d347b4deea0
韩洋 <731314840@qq.com>
2022-08-04 10:55:39 +0800

    create:Xiaojiejie.js 删除列表项(try) and回车事件冲突(catch)    simple Reach插件 快捷ccc直接生成模板

--- src/Xiaojiejie.js
+++ src/Xiaojiejie.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from "react";
-
+import './style.css'
 class Xiaojiejie extends Component {
   constructor(props) {
     super(props);
@@ -11,16 +11,28 @@ class Xiaojiejie extends Component {
   render() {
     return (
       <Fragment>
+        {/* 第一次写注释 */}
         <div>
+          <label htmlFor='hy' className="label">增加服务:</label>
           <input
+            id="hy"
             value={this.state.inputValue}
             onChange={this.inputChange.bind(this)}
             onKeyDown={this.KeyDown.bind(this)}
+            className='input'
           ></input>
           <button onClick={this.addList.bind(this)}>do you like</button>
           <ul>
             {this.state.list.map((item, index) => {
-              return <li key={index + item}>{item}</li>;
+              return (
+                <li
+                  key={index + item}
+                  onClick={this.deleteItem.bind(this, index)}
+                  dangerouslySetInnerHTML={{__html:item}}
+                >
+               
+                </li>
+              );
             })}
           </ul>
         </div>
@@ -37,7 +49,7 @@ class Xiaojiejie extends Component {
 
   //增加列表
   addList() {
-    if(!this.state.inputValue) return
+    if (!this.state.inputValue) return;
     this.setState({
       list: [...this.state.list, this.state.inputValue],
       inputValue: "",
@@ -45,11 +57,21 @@ class Xiaojiejie extends Component {
   }
   //回车事件
   KeyDown(e) {
+   
     var theEvent = window.event || e;
     var code = theEvent.keyCode || theEvent.which || theEvent.charCode;
     if (code === 13) {
       this.addList();
     }
+  }
+  //删除列表项
+  deleteItem(index) {
+    let list = this.state.list;
+    list.splice(index, 1);
+    this.setState({
+      list:list
+    });
+    // console.log(index);
   }
 }
 
